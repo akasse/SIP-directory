d6227f10a0b431272056f171b8538e721451a108
Thomas Boutry <thomas.boutry@x3rus.com>
2018-09-29 15:33:36 -0400

    Create server to listen port and accept connexion

--- server/server.py
+++ server/server.py
@@ -1,26 +1,63 @@
 #!/usr/bin/python
 #
-# Description : 
+# Description : Server to provide a SIP directory
 #
 # Author : thomas.boutry@x3rus.com
 # Licence : GPLv3
 # TODO :
+# -*- coding: utf-8 -*-
 ############################################################################
-
-# Argument :
-#   # Exclusion user
-#   # Exclusion repertoire
-#   # Disable validation jenkins build file in directory
 
 ###########
 # Modules #
 
+# import argparse                                    # Process command line argument
+# import sys
+# import os
+import socket
+import _thread
+# import re
 
-import argparse                                    # Process command line argument
-import sys
-import os
-import re
-from gitBuildTriggerValidation import gitBuildTriggerValidation
+
+class SIPdirectorySrv:
+    """
+    TODO: doc
+    """
+
+    def __init__(self, socketIP=socket.gethostname(), socketPort=1234):
+        """ TODO """
+
+        # TODO : Add socketIP and socketPort
+        print("Opening socket...")
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # bind the socket to a public host, and a well-known port
+        self.serversocket.bind((socketIP, socketPort))
+
+        # TODO : Load data
+
+    def processConnection(self, clientsocket, clientaddr):
+        """ TODO processConnection doc """
+        print("Accepted connection from: ", clientaddr)
+
+    def AcceptConnection(self, maxQueueConn=5):
+        """ TODO """
+
+        # become a server socket
+        self.serversocket.listen(5)
+
+        while True:
+            try:
+                print("Server is listening for connections\n")
+                clientsocket, clientaddr = self.serversocket.accept()
+                _thread.start_new_thread(
+                    self.processConnection, (clientsocket, clientaddr))
+            # Ctrl+C # FIXME: vraci "raise error(EBADF, 'Bad file descriptor')"
+            except KeyboardInterrupt:
+                print("Closing server socket...")
+                break
+
+# END class SIPdirectorySrv:
+
 
 #########
 # Main  #
@@ -28,5 +65,5 @@ from gitBuildTriggerValidation import gitBuildTriggerValidation
 
 if __name__ == '__main__':
 
-    # Set default value for last commit processed with success
- 
+        sipdir = SIPdirectorySrv()
+        sipdir.AcceptConnection()
